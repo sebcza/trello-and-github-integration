4eb3cefa16adf59670cd86172f8af90bb538fb8c
Sebastian Czarnecki <sebcza@outlook.com>
2014-09-02 15:29:23 +0200

    Replace php file by asp empty project

---
+++ git-trello-integration/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="git_trello_integration.MvcApplication" Language="C#" %>
---
+++ git-trello-integration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(git_trello_integration.Startup))]
+namespace git_trello_integration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
